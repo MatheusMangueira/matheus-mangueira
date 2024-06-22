dd78406383aad3dab87b500bd080b839356f8d4a
matheusmangueira <[matheusmangueira16@hotmail.com]clear>
2024-06-21 21:28:54 -0300

    feat: messages

--- src/components/messages/components/Messages.tsx
+++ src/components/messages/components/Messages.tsx
@@ -1,18 +1,11 @@
 import { useState } from "react";
+import { Message } from "../type";
 
-type MessagesProps = {
-  name: string;
-  message: string;
-  relationship: string;
-  date?: string;
-};
+interface MessagesProps {
+  messages: Message[];
+}
 
-export const Messages = ({
-  date,
-  message,
-  name,
-  relationship,
-}: MessagesProps) => {
+export const Messages = ({ messages }: MessagesProps) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const limit = 100;
 
@@ -20,27 +13,35 @@ export const Messages = ({
     setIsExpanded(!isExpanded);
   };
 
-  const shouldTruncate = message.length > limit;
-  const displayedMessage = isExpanded ? message : message.slice(0, limit);
+  return messages ? (
+    messages.map((item, index) => {
+      const shouldTruncate = item.message.length > limit;
+      const displayedMessage = isExpanded
+        ? item.message
+        : item.message.slice(0, limit);
 
-  return (
-    <div className="my-4">
-      <p className="text-lg">
-        <span className="font-bold">{name}</span> - {relationship}
-      </p>
-      <p className="text-gray-400 pb-2">{date}</p>
-      <p className="text-base break-words">
-        {displayedMessage}
-        {shouldTruncate && !isExpanded && "..."}
-      </p>
-      {shouldTruncate && (
-        <button
-          onClick={toggleExpansion}
-          className="text-gray-400 hover:text-gray-700 transition-all duration-300 ease-in-out text-sm font-semibold mt-1"
-        >
-          {isExpanded ? "Ver menos" : "Ver mais"}
-        </button>
-      )}
-    </div>
+      return (
+        <div key={index} className="my-4 pb-2 border-b-2 border-gray-200 ">
+          <p className="text-lg">
+            <span className="font-bold">{item.name}</span> - {item.relationship}
+          </p>
+          <p className="text-gray-400 pb-2">{item.date}</p>
+          <p className="text-base break-words">
+            {displayedMessage}
+            {shouldTruncate && !isExpanded && "..."}
+          </p>
+          {shouldTruncate && (
+            <button
+              onClick={toggleExpansion}
+              className="text-gray-400 hover:text-gray-700 transition-all duration-300 ease-in-out text-sm font-semibold mt-1"
+            >
+              {isExpanded ? "Ver menos" : "Ver mais"}
+            </button>
+          )}
+        </div>
+      );
+    })
+  ) : (
+    <div>Carregando...</div>
   );
 };
